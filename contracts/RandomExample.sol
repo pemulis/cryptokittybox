f08a2d50756bc3a2be975680bf15f0229592196d
John Shutt <john.d.shutt@gmail.com>
2018-03-01 16:43:34 -0800

    only return 1 random byte in RandomExample.sol

--- contracts/RandomExample.sol
+++ contracts/RandomExample.sol
@@ -34,7 +34,7 @@ contract RandomExample is usingOraclize {
             newRandomNumber_bytes(bytes(_result)); // this is the resulting random number (bytes)
 
             // for simplicity of use, let's also convert the random bytes to uint if we need
-            uint maxRange = 2**(8* 7); // this is the highest uint we want to get. It should never be greater than 2^(8*N), where N is the number of random bytes we had asked the datasource to return
+            uint maxRange = 2**(8* 1); // this is the highest uint we want to get. It should never be greater than 2^(8*N), where N is the number of random bytes we had asked the datasource to return
             uint randomNumber = uint(sha3(_result)) % maxRange; // this is an efficient way to get the uint out in the [0, maxRange] range
 
             newRandomNumber_uint(randomNumber); // this is the resulting random number (uint)
@@ -42,7 +42,7 @@ contract RandomExample is usingOraclize {
     }
 
     function update() payable {
-        uint N = 7; // number of random bytes we want the datasource to return
+        uint N = 1; // number of random bytes we want the datasource to return
         uint delay = 0; // number of seconds to wait before the execution takes place
         uint callbackGas = 200000; // amount of gas we want Oraclize to set for the callback function
         bytes32 queryId = oraclize_newRandomDSQuery(delay, N, callbackGas); // this function internally generates the correct oraclize_query and returns its queryId

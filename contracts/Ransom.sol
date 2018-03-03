c4f4fc855293d8af61c24171e2503f1f4853d982
John Shutt <john.d.shutt@gmail.com>
2018-03-03 11:40:52 -0800

    begin stubbing out Ransom contract

---
+++ contracts/Ransom.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.18;
+contract Ransom {
+  address public catnapper;
+  uint public price;
+  uint[] public safe;
+
+  event PayRansom(address backer, uint amount, uint catId);
+
+  /* Constructor function */
+  function Ransom (
+    address ifSuccessfulSendTo,
+    uint ransomPrice
+  ) {
+    catnapper = if ifSuccessfulSendTo;
+    price = ransomPrice * 1 ether;
+  }
+}
